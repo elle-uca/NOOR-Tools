5045d4e85f21eb3fa66021ecafb449ed275849ff
luke <luke@surino.org>
2025-12-27 11:15:23 +0100

    commit

---
+++ src/main/java/org/ln/noor/tools/directory/DirectoryTool.java
@@ -0,0 +1,32 @@
+package org.ln.noor.tools.directory;
+
+import javax.swing.JFrame;
+
+import org.ln.noor.core.tool.NoorTool;
+import org.ln.noor.directory.api.DirectoryToolLauncher;
+
+
+public class DirectoryTool implements NoorTool {
+
+    @SuppressWarnings("unused")
+	private final JFrame owner;
+
+    public DirectoryTool(JFrame owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String getId() {
+        return "directory";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Directory Tools";
+    }
+
+    @Override
+    public void open() {
+       DirectoryToolLauncher.open();
+    }
+}
